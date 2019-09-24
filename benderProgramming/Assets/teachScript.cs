a8362f50ae8f029f6ee74d254bffee6b545b4cfc
Jonathan Ribarro <Jcribarro@gmail.com>
2019-09-23 20:26:38 -0400

    Finished up the powerpoint ready for lesson

--- benderProgramming/Assets/teachScript.cs
+++ benderProgramming/Assets/teachScript.cs
@@ -1,37 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using System.IO;
 
-public class teachScript : MonoBehaviour {
+
+public class teachScript : MonoBehaviour
+{
 
     float currentSpeed;
     float maxSpeed = 10.0f;
     Rigidbody2D ourRB;
 
-	// Use this for initialization
-	void Start () {
-        ourRB = GetComponent<Rigidbody2D>();
-	}
-	
-
-    class Human
+    // Use this for initialization
+    void Start()
     {
-        int age;
-        string interests;
-        float height;
-        float weight;
-
-        void goOutside()
-        {
-            //Do outside stuff
-        }
-
-        int howManyDonutsHaveIEaten()
-        {
-            return 99;
-        }
-
+        ourRB = GetComponent<Rigidbody2D>();
     }
-
 
     void getPlayerInput()
     {
@@ -39,18 +24,54 @@ public class teachScript : MonoBehaviour {
         currentSpeed = CrossPlatformInputManager.GetAxis("Horizontal");
 
         ourRB.velocity = new Vector2(currentSpeed * maxSpeed, ourRB.velocity.y);
-    }
+    } 
 
-
-    private void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-
         getPlayerInput();
 
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
+
+
+
+
+
+
+
+//class Human
+//{
+//    int age;
+//    public string interests;
+//    float height;
+//    float weight;
+
+
+
+//    void goOutside()
+//    {
+//        //Do outside stuff
+//    }
+
+//    int howManyDonutsHaveIEaten()
+//    {
+//        return 99;
+//    }
+
+//    bool didIPooToday(bool didPoo)
+//    {
+//        return didPoo;
+//    }
+
+//}
+
+//class Bear
+//{
+//    Human h = new Human();
+
+//    void f()
+//    {
+//        h.interests = "";
+//    }
+//}
