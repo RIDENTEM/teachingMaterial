f1e4713a2c4be131d3cc2cfa2207362e1962ed44
Jonathan Ribarro <Jcribarro@gmail.com>
2020-01-16 17:13:40 -0500

    last commit

---
+++ benderProgramming/Assets/benderMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class benderMove : MonoBehaviour {
+
+    float currentSpeed;
+    float maxSpeed = 10.0f;
+    Rigidbody2D benderRB;
+
+	// Use this for initialization
+	void Start () {
+       benderRB = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       currentSpeed = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        benderRB.velocity = new Vector2(currentSpeed * maxSpeed, benderRB.velocity.y);
+	}
+}
